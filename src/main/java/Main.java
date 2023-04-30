dab3631f4fc5cd2da7c3474092fcf5002bd145ff
semiteplov <semiteplov@gmail.com>
2023-04-30 11:47:56 +0300

    Добавил проверку стоимости товара в формате числа

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,21 +7,23 @@ public class Main {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
         Calculator calculator = new Calculator();
         String input = "";
-        while(!input.equalsIgnoreCase("завершить")) {
+        while(!input.trim().equalsIgnoreCase("завершить")) {
             System.out.print("Введите название товара или команду \"Завершить\": ");
             input = scanner.nextLine();
-            if (!input.equalsIgnoreCase("завершить")) {
+            if (!input.trim().equalsIgnoreCase("завершить")) {
+                double price;
                 System.out.print("Введите стоимость товара в формате рубли.копейки (например, 10.45): ");
-                while(!scanner.hasNextDouble()) {
-                    System.out.println("Введите корректную стоимость товара (например, 10.45): ");
-                    scanner.next();
-                }
-                double price = scanner.nextDouble();
-                while (price < 0) {
-                    System.out.print("Введите положительное число: ");
+                do {
+                    while (!scanner.hasNextDouble()) {
+                        System.out.println("Введите числовое значение стоимости товара: ");
+                        scanner.next();
+                    }
                     price = scanner.nextDouble();
-                }
-                scanner.nextLine();
+                    scanner.nextLine();
+                    if (price < 0) {
+                        System.out.println("Стоймость товара не может быть отрицательной. Пожалуйста, введите положительное число: ");
+                    }
+                } while (price < 0);
                 Product product = new Product(input, price);
                 calculator.add(product);
                 System.out.println("Товар успешно добавлен в калькулятор!");
